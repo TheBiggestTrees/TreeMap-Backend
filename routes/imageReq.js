d1b6ee124ad632cd153b241f9bf309b31f9e6fa1
TheBiggestTrees <treeleaves229@gmail.com>
2024-04-29 00:26:49 -0500

    Update imageReq.js to use AWS SDK v3 for S3 integration

--- routes/imageReq.js
+++ routes/imageReq.js
@@ -1,34 +1,31 @@
 const router = require("express").Router();
-const AWS = require("aws-sdk");
-
-//get image from s3 bucket based on request body
-//setup aws s3 for image retrieval
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
 
 router.get("/:id", async (req, res) => {
-  const credentials = {
+  const credentials = defaultProvider({
     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
-  };
+  });
 
   const options = {
-    keyPrefix: "treeimages/",
-    bucket: "easytree",
-    region: "us-central-1",
-    successActionStatus: 201,
-    key: req.params.id,
+    Bucket: "easytree",
+    Key: `treeimages/${req.params.id}`,
   };
 
-  AWS.config.credentials = credentials;
-  AWS.config.region = options.region;
+  const client = new S3Client({ region: "us-central-1", credentials });
 
-  const ep = new AWS.Endpoint("s3.us-central-1.wasabisys.com");
-  const s3 = new AWS.S3({ endpoint: ep });
-
-  s3.getObject(options, (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).send({ message: "Image not found" });
-    }
-    res.status(200).send(data.Body);
-  });
+  try {
+    const data = await client.send(new GetObjectCommand(options));
+    const body = await new Promise((resolve, reject) => {
+      let body = "";
+      data.Body.on("data", (chunk) => (body += chunk));
+      data.Body.on("error", reject);
+      data.Body.on("end", () => resolve(body));
+    });
+    res.status(200).send(body);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: "Image not found" });
+  }
 });
