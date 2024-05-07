47c33cadf41524e54fd9527124317d7f9044cda1
TheBiggestTrees <treeleaves229@gmail.com>
2024-05-06 21:06:08 -0500

    chore: Implement pagination for retrieving sites in addSiteRoute.js

--- routes/addSiteRoute.js
+++ routes/addSiteRoute.js
@@ -4,15 +4,13 @@ const { Site, validate } = require("../models/sites");
 const { Tree } = require("../models/trees");
 //get all sites
 router.get("/", async (req, res) => {
+  const page = parseInt(req.query.page) || 1; // default to page 1
+  const limit = parseInt(req.query.limit) || 50; // default limit to 10 items
+  const skip = (page - 1) * limit;
 
-  {
-    const page = parseInt(req.query.page) || 1; // default to page 1
-    const limit = parseInt(req.query.limit) || 50; // default limit to 10 items
-    const skip = (page - 1) * limit;
-    
-    const total = await Site.countDocuments();
-    const pages = Math.ceil(total / limit);
-    const results = await Site.find().skip(skip).limit(limit);
+  const total = await Site.countDocuments();
+  const pages = Math.ceil(total / limit);
+  const results = await Site.find().skip(skip).limit(limit);
 
   res.status(200).send({
     total,
@@ -30,11 +28,16 @@ router.get("/trees/:id", async (req, res) => {
     const sites = await Site.findById(req.params.id);
     if (!sites) return res.status(404).send({ message: "Site not found" });
 
-    const validTreeIds = sites.properties.trees.filter(id => id);
+    const validTreeIds = sites.properties.trees.filter((id) => id);
     const records = await Tree.find({ _id: { $in: validTreeIds } });
 
-    console.log(records)
-    if (!records) return res.status(404).send({message: `No trees found for site ${sites.properties.siteID}`});
+    console.log(records);
+    if (!records)
+      return res
+        .status(404)
+        .send({
+          message: `No trees found for site ${sites.properties.siteID}`,
+        });
 
     return res.status(200).send({
       data: { trees: [...records] },
@@ -42,7 +45,7 @@ router.get("/trees/:id", async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ message: 'An error occurred on the server.' });
+    res.status(500).send({ message: "An error occurred on the server." });
   }
 });
 
@@ -52,13 +55,13 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send({ message: error.details[0].message });
 
   //increment siteID
-  const lastSite = await Site.find().sort({'_id':-1}).limit(1);
+  const lastSite = await Site.find().sort({ _id: -1 }).limit(1);
   if (lastSite.length === 0) {
     req.body.properties.siteID = 1;
   } else {
     req.body.properties.siteID = lastSite[0].properties.siteID + 1;
   }
-  
+
   const site = await Site(req.body).save();
   res.status(201).send({ data: site, message: "Site added successfully" });
 });
